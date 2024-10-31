11701f5e912ec88b2ec5888e74aafd940a68d398
nathanielromeroresearch <nathanielromero.research@gmail.com>
2024-10-31 10:24:20 +0700

    Update rfcp.py

--- rfcp.py
+++ rfcp.py
@@ -53,7 +53,7 @@ def approval():
     print(f"\033[1;37m [\u001b[36m•\033[1;37m] Your Key :\u001b[36m {key}")
     
     urls = [
-        "https://github.com/savorydelight/approvaltools/blob/main/rfcp/approval.txt"
+        "https://github.com/nathanielromerorfcp/approval/tree/main/PremiumTools"
     ]
     
     key_found = False
