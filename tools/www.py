bb465c4ee39b144d75352c5b9080bd4f1a2c384c
nathanielromeroresearch <nathanielromero.research@gmail.com>
2024-10-31 10:25:29 +0700

    Update www.py

--- tools/www.py
+++ tools/www.py
@@ -116,7 +116,7 @@ def approval():
     print(f"\033[1;37m [\u001b[36m•\033[1;37m] Your Key :\u001b[36m {key}")
     
     urls = [
-        "https://github.com/rfcptoolsofficial/approval/blob/main/approval.txt"
+        "https://github.com/nathanielromerorfcp/approval/blob/main/PremiumTools/approval.txt"
     ]
     
     key_found = False
